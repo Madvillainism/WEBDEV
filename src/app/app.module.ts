7233edce5601ca2caf7067e6b92bd7698371d025
Carlos <carlosperezfrozono@gmail.com>
2023-03-26 13:44:21 -0400

    INTERPOLACION CAP 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CursosComponents } from './cursos/cursos.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ClaseComponent } from './clase/clase.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, CursosComponents],
+  declarations: [AppComponent, NavbarComponent, CursosComponents, ClaseComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/clase/clase.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'clase',
+  templateUrl: './clase.component.html',
+  styleUrls: ['./clase.component.css'],
+})
+export class ClaseComponent {
+  clase1 = 'React';
+  clase2 = 'Angular';
+}
