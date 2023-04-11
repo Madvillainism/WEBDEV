9027595875a23f21c320887789c3a9edb4767e50
Carlos <carlosperezfrozono@gmail.com>
2023-04-11 00:25:49 -0400

    PROPERTY BINDING ANGULAR

--- src/app/clase/clase.component.ts
+++ src/app/clase/clase.component.ts
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
 export class ClaseComponent {
   clase1 = '';
   clase2 = ' ';
+  imagen = './gatico.jpg'; //INTENTO DE PROPERTY BINDING CON IMAGEN
+  userCheck = false; //PROPERTY BINDING CON CHECKBOX
 
   change(value: String) {}
 }
