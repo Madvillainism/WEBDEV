626ee2da53395502b1290300ef846c595cdef7fc
Carlos Perez <carlosperezph@hotmail.com>
2023-04-27 20:04:54 -0400

    CALCU ANGULAR SUMA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'calculator';
+  title = 'CALQLO';
+  num1: number = 0;
+  num2: number = 0;
+  resul: number = 0;
+
+  sumar(): void {
+    this.resul = this.num1 * 1 + this.num2 * 1;
+  }
 }
